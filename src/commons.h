d7e0a42b03e643ff42f67f88cd2aa2d52c4cad78
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2022-02-15 15:34:29 +0100

    + commons.h, gui_qt6.cpp

---
+++ src/commons.h
@@ -0,0 +1,12 @@
+//
+//  commons.h
+//  e2-sat-editor
+//
+//  Created by Leonardo Laureti on 15/02/22.
+//
+
+#ifndef commons_h
+#define commons_h
+
+
+#endif /* commons_h */
---
+++ src/gui_qt6.h
@@ -0,0 +1,12 @@
+//
+//  gui_qt6.h
+//  e2-sat-editor
+//
+//  Created by Leonardo Laureti on 15/02/22.
+//
+
+#ifndef gui_qt6_h
+#define gui_qt6_h
+
+
+#endif /* gui_qt6_h */
