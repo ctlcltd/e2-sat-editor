09024dd1af748932299460f58204aa957e14f956
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2023-05-12 18:30:31 +0200

    testing snap, snapcraft, CMake subdirectory with e2se-cli

--- scripts/snapcraft-test.sh
+++ scripts/snapcraft-test.sh
@@ -4,6 +4,16 @@
 
 cd dist/linux
 
+# rm e2-sat-editor_*.snap
+# snap list
+# snap remove e2-sat-editor
+
 snapcraft clean
 snapcraft --debug
+
+# snap install --devmode e2-sat-editor_*.snap
+# snap run --shell e2-sat-editor.e2-sat-editor
+
+# e2-sat-editor
+# exit
 
