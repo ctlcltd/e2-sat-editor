395d7eebdbcc25a133b2c51c631ada3be7e65f5a
Leonardo Laureti <204957+leolweb@users.noreply.github.com>
2024-10-19 18:32:50 +0200

    export dist icons

--- scripts/icons.sh
+++ scripts/icons.sh
@@ -2,7 +2,7 @@
 # Convert icons
 # - iconutil (macOS)
 # - straight copy (*ux)
-# - IMagick (Windows)
+# - ImageMagick (Windows)
 # 
 
 usage () {
@@ -31,7 +31,7 @@ icons_win () {
 
 	cwt
 
-	convert res/e2-sat-editor/icon-512.png res/e2-sat-editor/icon-256.png res/e2-sat-editor/icon-128.png res/e2-sat-editor/icon-64.png res/e2-sat-editor/icon-48.png res/e2-sat-editor/icon-32.png dist/windows/e2-sat-editor.ico
+	magick res/e2-sat-editor/icon-256.png res/e2-sat-editor/icon-128.png res/e2-sat-editor/icon-64.png res/e2-sat-editor/icon-48.png res/e2-sat-editor/icon-32.png dist/windows/e2-sat-editor.ico
 }
 
 icons_macx () {
@@ -40,12 +40,11 @@ icons_macx () {
 	cwt
 
 	mkdir -p dist/macos/icons.iconset
-	cp res/e2-sat-editor/icon-{512,256,128,64,48,32}.png dist/macos/icons.iconset
+	cp res/e2-sat-editor/icon-{512,256,128,64,32}.png dist/macos/icons.iconset
 	mv dist/macos/icons.iconset/icon-512.png dist/macos/icons.iconset/icon_512x512.png
 	mv dist/macos/icons.iconset/icon-256.png dist/macos/icons.iconset/icon_256x256.png
 	mv dist/macos/icons.iconset/icon-128.png dist/macos/icons.iconset/icon_128x128.png
 	mv dist/macos/icons.iconset/icon-64.png dist/macos/icons.iconset/icon_64x64.png
-	mv dist/macos/icons.iconset/icon-48.png dist/macos/icons.iconset/icon_48x48.png
 	mv dist/macos/icons.iconset/icon-32.png dist/macos/icons.iconset/icon_32x32.png
 
 	iconutil -c icns dist/macos/icons.iconset -o dist/macos/e2-sat-editor.icns
@@ -60,7 +59,7 @@ icons_ux () {
 
 	mkdir -p dist/xdg/icons/{scalable,512x512,256x256,128x128,64x64,48x48,32x32}
 	cp res/e2-sat-editor/icon.svg dist/xdg/icons/scalable/e2-sat-editor.svg
-	cp res/e2-sat-editor/icon-512.png dist/xdg/icons/512,512/e2-sat-editor.png
+	cp res/e2-sat-editor/icon-512.png dist/xdg/icons/512x512/e2-sat-editor.png
 	cp res/e2-sat-editor/icon-256.png dist/xdg/icons/256x256/e2-sat-editor.png
 	cp res/e2-sat-editor/icon-128.png dist/xdg/icons/128x128/e2-sat-editor.png
 	cp res/e2-sat-editor/icon-64.png dist/xdg/icons/64x64/e2-sat-editor.png
